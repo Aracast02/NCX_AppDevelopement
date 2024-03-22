6a99de7be4d359f7a83b31cb0412b75c938319dc
Arantza Castro Dssavre <ary.castro.dessavre@gmail.com>
2024-03-22 15:15:41 +0100

    Initial Commit

---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresAndAlgorithmsApp.swift
+//  DataStructuresAndAlgorithms
+//
+//  Created by Arantza Castro Dessavre on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresAndAlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
