efd099ff68be532df247c5310c436782d0d35909
Arantza Castro Dssavre <ary.castro.dessavre@gmail.com>
2024-03-25 14:52:35 +0100

    queue and linked lists !!

---
+++ DataStructuresAndAlgorithms/Queue.swift
@@ -0,0 +1,58 @@
+//
+//  Queue.swift
+//  DataStructuresAndAlgorithms
+//
+//  Created by Arantza Castro Dessavre on 22/03/24.
+//
+
+import Foundation
+import Dispatch
+
+public class Queue<T>
+{
+    fileprivate var list = LinkedList<T>()
+    
+    public var isEmpty: Bool { return list.isEmpty }
+    
+    private var lock = DispatchGroup.init()
+    
+    public init()
+    {
+        //
+    }
+    
+    public func enqueue(_ element: T)
+    {
+        lock.enter()
+        list.append(value: element)
+        lock.leave()
+    }
+    
+    public func dequeue() -> T?
+    {
+        lock.enter()
+        guard !list.isEmpty, let element = list.first
+        else
+        {
+            return nil
+        }
+        
+        let poppedValue = list.pop(node: element)
+        lock.leave()
+        
+        return poppedValue
+    }
+    
+    public func peek() -> T?
+    {
+        return list.first?.value
+    }
+}
+
+extension Queue: CustomStringConvertible
+{
+    public var description: String
+    {
+        return list.description
+    }
+}
